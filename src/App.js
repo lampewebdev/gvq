6bc398c7500a09be717a52057c184e026e3cec4f
Michael Lazarski <michael.lazarski@quantilope.com>
2019-02-22 21:45:27 +0200

    added next button and added more quotes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import "./App.css";
 
 class App extends Component {
   state = {
-    imageIsReady: false
+    image: "",
+    imageIsReady: false,
+    quote: ""
   };
+
   componentDidMount() {
     const img = new Image();
     const viewPortWidth = window.innerWidth;
@@ -12,7 +15,7 @@ class App extends Component {
     img.src = `https://source.unsplash.com/${viewPortWidth}x${viewPortHeight}/?nature`; // by setting an src, you trigger browser download
     img.setAttribute("crossOrigin", "anonymous"); // works for me
     img.onload = () => {
-      console.log('img loaded...')
+      console.log("img loaded...");
       var canvas = document.createElement("canvas");
       canvas.width = img.width;
       canvas.height = img.height;
@@ -21,14 +24,16 @@ class App extends Component {
 
       var dataURL = canvas.toDataURL("image/jpg", 0.8);
       // when it finishes loading, update the component state
-      console.log('setting background...')
+      console.log("setting background...");
       this.setState({
         image: dataURL,
         imageIsReady: true
       });
+      this.getRandomQuote();
     };
   }
-  getRandomQuote() {
+
+  getRandomQuote = () => {
     const quotes = [
       "The truth is that finding happiness in what you do every day is so imperative.",
       "No matter what you do, your job is to tell your story",
@@ -46,19 +51,37 @@ class App extends Component {
       "Go do your thing. You're gonna die.",
       "It's very very simple, put yourself in a position to succeed.",
       "You can't be heard if you're not communicating.",
+      "If you live for the weekends and vacations, your shit is broken.",
+      "Stop whining. Start hustling.",
+      "If you want to be anomaly, you gotta act like one.",
+      "It‘s not about how much sleep you get. It’s About what you do when you are awake.",
+      "I Lose on the daily basis but I am winning on the decade basis",
+      "Don’t dwell on your weakness everyone else is already doing that for you.",
+      "If it was so damn easy, don’t you think everyone would be a millionaire.",
+      `“I Wish” is a dangerous way to start a sentence.`,
+      "You can market your ass off, but if your product sucks, you’re dead.",
       "Whether you’re 9 or 90, stop trying to fix the things you’re bad at, and focus on the things you’re good at."
     ];
-    return quotes[Math.floor(Math.random() * quotes.length)];
+
+    this.setState({
+      quote: quotes[Math.floor(Math.random() * quotes.length)]
+    });
   }
+
   render() {
-    const { image, imageIsReady } = this.state;
+    const { image, imageIsReady, quote } = this.state;
     return (
       <div>
         <div className="header"> GVQ </div>
 
         <div className="App" style={{ backgroundImage: `url(${image})` }}>
           {imageIsReady ? (
-            <div className="content">{this.getRandomQuote()}</div>
+            <React.Fragment>
+              <div className="content">{quote}</div>
+              <div className="newQuote" onClick={this.getRandomQuote}>
+                Next quote!
+              </div>
+            </React.Fragment>
           ) : (
             <div className="lds-ripple">
               <div />
